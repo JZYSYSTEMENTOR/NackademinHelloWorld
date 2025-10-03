f55f66c7f017ab1baea67b6716f6eb9c521ba738
JZYSYSTEMENTOR <zhengyao.jiang@systementor.se>
2025-10-03 14:27:38 +0200

    change Hej Världen to Hello World

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hej Världen!");
+        System.out.println("Hello World");
     }
 }
